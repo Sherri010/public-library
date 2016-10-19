617acbd479f5ef49c88d57e44a52902960316b4d
sherri010 <aminshahidi.sh@gmail.com>
2016-10-19 14:00:44 -0700

    init the app/ adding new user form

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ root to: 'users#index'
+
+ get '/users', to: 'users#index', as: 'users'
+ get '/users/new', to: 'users#new', as: 'new_user'
+end
